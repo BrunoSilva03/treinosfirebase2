3626902ee301025fa4c65d0c155a61043b62a60b
Bruno Silva <bruno5ssilva02@gmail.com>
2024-01-12 15:58:44 -0300

    resolução definitiva de bug

--- src/InputComFoto.js
+++ src/InputComFoto.js
@@ -1,7 +1,32 @@
 import styles from './InputComFoto.module.css';
-import camera  from './images/camera30x30.png'
+import camera  from './images/camera30x30.png';
+
+import { useEffect } from 'react';
 
 function InputComFoto() {
+    'use strict'
+
+    //Código dentro do useEffect para não dar erro.
+    //Se o input type estiver abrindo mais de uma vez exclua o StrictMode no index.js
+    //Ele simula o comportamento de montagem e desmontagem do componente e as vezes ele duplica algumas paradas e ações.
+    useEffect(() => {
+
+        let photo = document.getElementById('imgPhoto');
+        let file = document.getElementById('flImage');
+        let circulo = document.getElementById('imageContainer');
+        
+        photo.addEventListener('click', () => {
+            console.log('entrou 1');
+            file.click();
+            return;
+        });
+        
+    }, [])
+    // circulo.addEventListener('click', () => {
+    //     file.click();
+    // });
+
+
     return(
         <>
 
